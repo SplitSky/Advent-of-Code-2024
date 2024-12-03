06fa39d2caa5b7d5ea542f4b8fe097651209d53a
SplitSky <opop.neska60@gmail.com>
2024-12-03 15:36:01 +0000

    Finished day 3

--- Day_3/src/main.rs
+++ Day_3/src/main.rs
@@ -4,7 +4,7 @@ use std::io::{self, BufRead};
 
 fn unpack_string(input: &str) -> Vec<&str> {
     // this takes in an input string and returns a vector of multiples
-    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let re = Regex::new(r"do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)").unwrap();
 
     let matches: Vec<&str> = re
         .find_iter(input) // Find all matches in the input string
@@ -16,6 +16,7 @@ fn unpack_string(input: &str) -> Vec<&str> {
 
 fn unpack_mul(input: &str) -> i32 {
     // unpack and return the multiple
+    println!("Internal input check: {}", input);
     let inner_content = &input[4..input.len() - 1];
     let parts: Vec<&str> = inner_content.split(',').collect();
     let num1: i32 = parts[0].trim().parse().unwrap();
@@ -29,12 +30,22 @@ fn main() -> io::Result<()> {
     // loop over a line in a file and process it and add to the sum
     let file = File::open("input.txt")?;
     let reader = io::BufReader::new(file);
+    let mut keep_going: bool = true;
     for line in reader.lines() {
         let line = line?;
         let operations: Vec<&str> = unpack_string(&line);
         for operation in operations {
-            // loop over each multiple
-            sum += unpack_mul(operation);
+            // check whether it's a do or don't
+            println!("operation {}", operation);
+            println!("result of comparison {}", operation == "do()");
+            println!("result of comparison {}", operation == "don't()");
+            if operation == "do()" {
+                keep_going = true;
+            } else if operation == "don't()" {
+                keep_going = false;
+            } else if keep_going == true {
+                sum += unpack_mul(operation);
+            } // loop over each multiple
         }
     }
     println!("The total is: {}", sum);

22365eb1db7b370a6dc465d27820c9051cc7b953
SplitSky <opop.neska60@gmail.com>
2024-12-04 23:47:03 +0000

    Wrote out method for day 4 puzzle part 1

---
+++ Day_4/src/main.rs
@@ -0,0 +1,15 @@
+fn check_square(square: Vec<[&str; 4]>) {
+    // checks the array of strings
+    let mut x = 0; // x-coordinate
+    let mut y = 0; // y-coordinate
+    let word = "XMAS";
+    let word2 = "SMAX";
+    // perform check vertically on the first row - two words
+    // perform check horizontally on the first column - two words
+    // perform a check diagonally on the left to right - two words
+    // perform a check diagonally on the right to left - two words
+}
+
+fn main() {
+    println!("Hello, world!");
+}

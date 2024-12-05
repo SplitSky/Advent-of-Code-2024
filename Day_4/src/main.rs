bfbe34341ffd2af0e183ea5e256091448b985a29
SplitSky <opop.neska60@gmail.com>
2024-12-05 14:58:01 +0000

    added details to the implementation of the puzzle on day 4

--- Day_4/src/main.rs
+++ Day_4/src/main.rs
@@ -10,6 +10,31 @@ fn check_square(square: Vec<[&str; 4]>) {
     // perform a check diagonally on the right to left - two words
 }
 
+fn DPS(data: Vec<Vec<&str>>, word: Vec<&str>) {
+    // directions
+    //  word = ["X", "M", "A", "S"];
+    for i -1..=1 {
+        for j -1..=1 {
+            // loops over all directions
+            let mut letter_counter = 0;
+            let stop = false;
+            // check letter matches
+            // if 
+            //      Move counter by one
+            //      Check if it's at the end of the word
+            //      else -> stop
+            //  else
+            //      stop
+            //  check move criteria
+            //  if out of bounds
+            //      stop
+            //  else
+            //      nothing
+
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }

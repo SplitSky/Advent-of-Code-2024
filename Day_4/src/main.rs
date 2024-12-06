e69e170518c367d01fbafd93815631fbc00baf05
SplitSky <opop.neska60@gmail.com>
2024-12-06 15:19:43 +0000

    synch:

--- Day_4/src/main.rs
+++ Day_4/src/main.rs
@@ -1,4 +1,4 @@
-fn check_square(square: Vec<[&str; 4]>) {
+ n check_square(square: Vec<[&str; 4]>) {
     // checks the array of strings
     let mut x = 0; // x-coordinate
     let mut y = 0; // y-coordinate
@@ -13,12 +13,23 @@ fn check_square(square: Vec<[&str; 4]>) {
 fn DPS(data: Vec<Vec<&str>>, word: Vec<&str>) {
     // directions
     //  word = ["X", "M", "A", "S"];
+    for i 
+
     for i -1..=1 {
         for j -1..=1 {
             // loops over all directions
             let mut letter_counter = 0;
             let stop = false;
             // check letter matches
+            if data[i][j] = word[letter_counter] {
+                letter_counter += 1;
+                // check if it's end of the word
+                if letter_counter < word.len() - 1 {
+                     // continue
+                } else if letter_counter == word.len() -1 {
+                    // check if the last letter is correct
+                }
+            }
             // if 
             //      Move counter by one
             //      Check if it's at the end of the word

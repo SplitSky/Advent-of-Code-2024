08942ff5c6a8b2d9c82d428baf4c8c1b92ecd4b4
SplitSky <opop.neska60@gmail.com>
2024-12-10 12:57:42 +0000

    first idea on how to implement

--- Day_4/src/main.rs
+++ Day_4/src/main.rs
@@ -1,39 +1,51 @@
-fn depth_first_search(data: Vec<Vec<&str>>, word: Vec<&str>) {
-    // directions
-    //  word = ["X", "M", "A", "S"];
-
-    for i in 0..2 {
-        for j in 0..2 {
-            // loops over all directions
-            // i-1 is the loop
-            // j-1 is the loop
-            let mut letter_counter = 0;
-            let stop = false;
-            // check letter matches
-            if data[i][j] == word[letter_counter] {
-                letter_counter += 1;
-                // check if it's end of the word
-                if letter_counter < word.len() - 1 {
-                    // continue
-                } else if letter_counter == word.len() - 1 {
-                    // check if the last letter is correct
+fn dps(data: Vec<Vec<&str>>, word: Vec<&str>) {
+    // loop over all tiles
+    let mut word_counter = 0;
+    for x in 0..data.len() {
+        for y in 0..data[x].len() {
+            // go over all directions
+            for i in 0..2 {
+                let mut curr_x = x;
+                let mut curr_y = y;
+                // declare current position variables
+                for j in 0..2 {
+                    // loop over directions
+                    // check the letter agrees
+                    let mut stop = false;
+                    let mut letter_counter = 0;
+                    while stop != true {
+                        // logic for comparisons
+                        // check the letter matches
+                        if word[letter_counter] != data[x][y] {
+                            stop = true;
+                        } else {
+                            letter_counter += 1; // move to the next letter in the word
+                                                 // move the direction
+                                                 // check if it's the end of the word
+                            if letter_counter == word.len() {
+                                // success word has been found
+                                word_counter += 1;
+                            }
+                            // make a move!
+                            curr_x += i-1;
+                            curr_y += j-1;
+                            // check out of bounds
+                            if curr_x < 0 || curr_x > data[x].len() {
+                                // stop
+                                stop = true;
+                            }
+                            if curr_y < 0 || curr_y > data.len() {
+                                stop = true;
+                            }
+                            // else keep going
+                        }
+                    }
                 }
             }
-            // if
-            //      Move counter by one
-            //      Check if it's at the end of the word
-            //      else -> stop
-            //  else
-            //      stop
-            //  check move criteria
-            //  if out of bounds
-            //      stop
-            //  else
-            //      nothing
         }
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let data_in = [[]]
 }

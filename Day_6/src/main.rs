3bf93b89047b73ee2ee9bb16bcea8eed38d3ee87
SplitSky <opop.neska60@gmail.com>
2024-12-11 19:32:16 +0000

    skeleton and plan for implementation

--- Day_6/src/main.rs
+++ Day_6/src/main.rs
@@ -1,5 +1,16 @@
 // get the board set up
-// find the guard:wq
+// make struct for guard
+// make a while loop and on every field
+// 1. check what it is
+//  if empty
+//      nothing
+//  if obstacle
+//      rotate 90 degrees
+//      nothing
+//
+//  make move
+//  check out of bounds conditions
+//  x_coord and y_coord
 
 fn main() {
     println!("Hello, world!");

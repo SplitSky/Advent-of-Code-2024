cb53ede058ef0d50cf2368bc58fe99f4d5c29a99
SplitSky <opop.neska60@gmail.com>
2024-12-13 01:08:52 +0000

    edits

--- Day_6/src/main.rs
+++ Day_6/src/main.rs
@@ -12,6 +12,27 @@
 //  check out of bounds conditions
 //  x_coord and y_coord
 
+struct Guard {
+    x_pos: i32,
+    y_pos: i32,
+    direction: (i8, i8),
+}
+
+impl Guard {
+    fn new(&mut self, x_pos: i32, y_pos: i32, direction: (i8, i8)) {
+        // initialise the guard struct
+        self.x_pos = x_pos;
+        self.y_pos = y_pos;
+        self.direction = direction;
+    }
+    fn rotate(&mut self) {
+        // matrix multiplication
+        //[0, -1] [x]= [-x]
+        //[1,  0] [y]= [ y]
+        self.x_pos = -self.x_pos;
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
